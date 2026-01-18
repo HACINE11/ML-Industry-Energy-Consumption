d3a6cdbc7a7997876a8d094fceb2c2dc4f9537aa
meherfe <meher.fe08@gmail.com>
2026-01-18 21:21:12 +0100

    Add Linear Regression model

---
+++ src/models/linear.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+
+class LinearModel:
+    def __init__(self):
+        self.model = LinearRegression()
+
+    def train(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
+
+    def evaluate(self, X_test, y_test):
+        y_pred = self.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+
+        return {
+            "MAE": mean_absolute_error(y_test, y_pred),
+            "MSE": mse,
+            "RMSE": np.sqrt(mse),
+            "R2": r2_score(y_test, y_pred),
+        }
