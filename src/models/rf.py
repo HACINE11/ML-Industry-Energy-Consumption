a9f835686e02e13633a33c7bc4514fe783f63d2d
Ahmed DHIB <dhib.ahmed@esprit.tn>
2026-01-19 01:22:31 +0100

    feat: add RandomForestRegressor model

--- src/models/rf.py
+++ src/models/rf.py
@@ -1,16 +1,18 @@
 from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestRegressor
 
+
 def build_model(preprocessor):
     return Pipeline([
         ("pre", preprocessor),
-        ("model", RandomForestRegressor(random_state=42))
+        ("model", RandomForestRegressor(random_state=42, n_jobs=-1)),
     ])
 
+
 param_grid = {
-    "model__n_estimators": [100, 200, 500],
-    "model__max_depth": [None, 10, 20, 30],
-    "model__min_samples_split": [2, 5, 10],
-    "model__min_samples_leaf": [1, 2, 4],
+    "model__n_estimators": [200, 500],
+    "model__max_depth": [None, 10, 20],
+    "model__min_samples_split": [2, 5],
+    "model__min_samples_leaf": [1, 2],
     "model__max_features": ["sqrt", "log2"],
 }
