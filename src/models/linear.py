c8f3477b9e1743bd359422c72905753e2944e303
meherfe <meher.fe08@gmail.com>
2026-01-19 13:47:05 +0100

    Register linear model in model registry

--- src/models/linear.py
+++ src/models/linear.py
@@ -1,25 +1,14 @@
-import numpy as np
+from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+MODEL_NAME = "linear"
 
 
-class LinearModel:
-    def __init__(self):
-        self.model = LinearRegression()
+def build_model(preprocessor):
+    return Pipeline([
+        ("pre", preprocessor),
+        ("model", LinearRegression()),
+    ])
 
-    def train(self, X_train, y_train):
-        self.model.fit(X_train, y_train)
 
-    def predict(self, X_test):
-        return self.model.predict(X_test)
-
-    def evaluate(self, X_test, y_test):
-        y_pred = self.predict(X_test)
-        mse = mean_squared_error(y_test, y_pred)
-
-        return {
-            "MAE": mean_absolute_error(y_test, y_pred),
-            "MSE": mse,
-            "RMSE": np.sqrt(mse),
-            "R2": r2_score(y_test, y_pred),
-        }
+param_grid = {}
